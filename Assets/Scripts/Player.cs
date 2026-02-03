9df7893834b30873e29d1719c7e008285b04e1b9
GhostToast <69281498+GhostToasty@users.noreply.github.com>
2026-02-03 14:41:20 -0800

    player controls successful
    - player controls now move independently (WASD and arrow keys)
    - script for player controls complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,58 +6,27 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementY;
-    public float forceStrength = 1f;
+    public float speed = 1f;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Debug.Log("start");
     }
-
     
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     // if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
-    //     // {
-    //     //     Vector3 force = new Vector3(0f, forceStrength, 0f);
-    //     //     Rigidbody rBody = GetComponent<Rigidbody>();
-    //     //     rBody.AddForce(force, ForceMode.Force);
-            
-    //     //     //transform makes collision jittery since script runs before rigid body 
-    //     //     // transform.position += new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
-    //     // }
-
-    //     // if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
-    //     // {
-    //     //     Vector3 force = new Vector3(0f, -forceStrength, 0f);
-    //     //     Rigidbody rBody = GetComponent<Rigidbody>();
-    //     //     rBody.AddForce(force, ForceMode.Force);
-    //     //     // transform.position += new Vector3(0f, 0f, paddleSpeed) * Time.deltaTime;
-    //     // }
-
-    // }
-
-    void OnMove(InputValue movementValue)
+    void OnMovement(InputValue movementValue)
     {
-        Debug.Log("start run");
         Vector2 movementVector = movementValue.Get<Vector2>();
         
         movementY = movementVector.y;
-        Debug.Log("running");
     }
 
     private void FixedUpdate()
     {
-        Vector3 force = new Vector3(0.0f, movementY * forceStrength, 0.0f);
-        
-        rb.AddForce(force, ForceMode.Force);
+        Vector3 movement = new Vector3(0.0f, movementY, 0.0f);
+    
+        rb.linearVelocity = movement * speed;
     }
 
 }
-
-//acceleration: ignores mass, handles time
-//force: takes mass and acceleration into account, handles time
-//impulse: for current frame, applies all force immediately, accounts for mass, doesn't handle time
-//velocity change: for current frame, applies all force immediately, doesn't account for mass, doesn't handle time 
